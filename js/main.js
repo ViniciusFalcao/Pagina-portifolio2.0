8e75ef5809e9d068c939db62bda5dc927e327521
Vinicius Canuto Falcão <viniciusfalcao82@gmail.com>
2022-06-21 19:35:58 -0300

    terminei cards

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
 var seta_sobre=document.querySelector('.seta_sobre');
+var sobre_nav=document.querySelector('.sobre-nav');
+var formacoes_nav=document.querySelector('.formacoes-nav')
+sobre_nav.addEventListener('click' ,function () {
+    location.reload()
+    
+})
+formacoes_nav.addEventListener('click',function () {
+    var section_sobre=document.querySelector('.sobre');
+    var section_formacoes=document.querySelector('.formaçoes')
+    var div_prox=document.querySelector('.div-prox')
+    div_prox.classList.add('display-none')
+
+    
+
+    section_sobre.classList.add('display-none')
+
+    section_formacoes.classList.remove('display-none')
+
+    
+})
 
 
 seta_sobre.addEventListener('click',function () {
     var section_sobre=document.querySelector('.sobre');
     var section_formacoes=document.querySelector('.formaçoes')
+    var div_prox=document.querySelector('.div-prox')
 
     section_sobre.classList.add('display-none')
 
     section_formacoes.classList.remove('display-none')
+    div_prox.classList.add('display-none')
+
     
 })
 
