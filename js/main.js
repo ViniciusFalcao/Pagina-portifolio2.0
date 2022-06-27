1b0017602c0573415129cc3ac0e90ae872a5714f
Vinicius Canuto Falcão <viniciusfalcao82@gmail.com>
2022-06-27 17:05:10 -0300

    ----

--- js/main.js
+++ js/main.js
@@ -1,39 +1,30 @@
 var seta_sobre=document.querySelector('.seta_sobre');
 var sobre_nav=document.querySelector('.sobre-nav');
 var formacoes_nav=document.querySelector('.formacoes-nav')
+var projetos_nav=document.querySelector('.projetos-nav')
+var contato_nav=document.querySelector('.contato-nav')
 var seta_forma=document.querySelector('.seta-forma');
 var section_formacoes=document.querySelector('.formaçoes')
 var section_projetos=document.querySelector('.projetos')
 var section_sobre=document.querySelector('.sobre');
+var section_contato=document.querySelector('.contato')
 var div_prox=document.querySelector('.div-prox');
+contato_nav.addEventListener('click',function () {
+    section_contato.scrollIntoView()
+    
+})
 
 
 sobre_nav.addEventListener('click' ,function () {
-    location.reload()
+      scrollTo(0,0)
     
 })
 formacoes_nav.addEventListener('click',function () {
-    div_prox.classList.add('display-none')
-    section_sobre.classList.add('display-none')
-    section_formacoes.classList.remove('display-none')
-    section_projetos.classList.add('display-none')
+   section_formacoes.scrollIntoView()
 
     
 })
 
-
-seta_sobre.addEventListener('click',function () {
-    section_sobre.classList.add('display-none')
-    section_formacoes.classList.remove('display-none')
-    div_prox.classList.add('display-none')
-
-    
-})
-
-seta_forma.addEventListener('click',function () {
-    section_formacoes.classList.add('display-none')
-    section_projetos.classList.remove('display-none')
-})
 
 var imagens_proj=document.querySelectorAll('.img-projetos')
 var info_projetos=document.querySelectorAll('.info_projetos')
@@ -85,4 +76,10 @@ for (let index = 0; index < card.length; index++) {
     
     
 }
+var projetos_nav=document.querySelector('.projetos-nav');
+projetos_nav.addEventListener('click',function () {
+    section_projetos.scrollIntoView()
+    
+})
+
 
